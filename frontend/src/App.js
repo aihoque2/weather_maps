68b9ac3b1c314a41960cf8b3d05d58b8d9468c69
Ammar Hoque <aihoque2@illinois.edu>
2024-08-22 21:34:05 -0400

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Put some map of some weather data here or something idk
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Git Gud
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
