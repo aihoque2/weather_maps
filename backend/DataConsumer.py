9758dc1336386671db252a14e00ba67fe830f88e
Ammar Hoque <aihoque2@illinois.edu>
2024-08-22 21:18:11 -0400

    add new dir

--- backend/DataConsumer.py
+++ backend/DataConsumer.py
@@ -4,4 +4,8 @@ take the data from the kafka topic stream
 
 store it in some database or some backend accessible
 via some visualization API embedded by React.
+
+https://www.weatherapi.com/unverified.aspx?tagid=01202408235201010823
+
+
 """
