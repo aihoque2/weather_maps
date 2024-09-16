45598e6cbf6d254e7cf01393aa9b972813bedd7a
Ammar Hoque <aihoque2@illinois.edu>
2024-09-16 19:00:47 -0400

    best practices

--- backend/DataProducer.py
+++ backend/DataProducer.py
@@ -11,24 +11,40 @@ https://www.weatherapi.com/unverified.aspx?tagid=01202408235201010823
 
 import requests
 from confluent_kafka import Proudcer
-import json
+import yaml
+
+ciites = ["Anchorage, AK", "Los Angeles, CA", "San Diego, CA", "Los Angeles, CA", "San Francisco, CA", "Pittsburgh", "St. Louis"]
 
 class DataProducer:
     """
     TODO: turn this into an ABC?
+
     """
     def __init__(self):
         self.api_key = ''
+        # I don't want to expose the api key file
+        api_file = "auth.yaml"
+        try:
+            with(open(api_file, 'r') as f):
+                config = yaml.load(f)
+                self.api_key = config[weather_data_api]
+
+        except FileNotFoundError:
+            print("'%s' file not found:" % filename)
+
         self.config = {}
         self.topic = ''
 
-        self.producer = Producer()
+        producer_conf = {'bootstrap.servers': 'localhost:9092'}
+        self.producer = Producer(producer_conf)     
+        weather_topic = ''
 
     def get_data_from_city(self, city):
         """
         weather data from one part of the city
-        just use requests and do a call
+        just use REQUESTS and do a call
         """
+
         pass
 
     def get_current_data(self, cities):
