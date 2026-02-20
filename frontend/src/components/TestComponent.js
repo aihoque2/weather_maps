2c68dcc45687e94af51bb44bf1499d7d861653f2
Ammar Hoque <aihoque2@illinois.edu>
2026-02-19 21:49:02 -0500

    posting up data

--- frontend/src/components/TestComponent.js
+++ frontend/src/components/TestComponent.js
@@ -8,23 +8,19 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_HUMIDITY_BY_CITY_STATE} from "../db/queries.js"; // Import the query
 
-import yaml from "js-yaml";
-
-
 export default function TestHumidity(props) {
-
-    const cities_dict = yaml.load("./cities.yaml")
     
     const city_name = "St. Louis, MO";
     const state_name = "Missouri";
 
     const {loading, error, data} = useQuery(GET_HUMIDITY_BY_CITY_STATE, 
-                                            {variables: {city_name, state_name}});
+                                            {variables: {city: city_name, state: state_name}});
 
 
     if (loading) return <h1>LOADING....</h1>;
     if (error) return <h1>`Error! ${error.message}`</h1>;
 
+    console.log("here's data: ", data.getMostRecentHumidity)
     return<h1>{data.getMostRecentHumidity.humidity}</h1>
 
 }
