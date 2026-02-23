0394400cc0346366201bb76b59142a6d22a163c7
Ammar Hoque <aihoque2@illinois.edu>
2026-02-22 22:00:29 -0500

    fixed bug in TestComponent.js

--- frontend/src/components/TestComponent.js
+++ frontend/src/components/TestComponent.js
@@ -18,15 +18,15 @@ export default function TestHumidity(props) {
                                             {variables: {city: city_name, state: state_name}});
 
     if (loading) return <h1>LOADING....</h1>;
-    if (error) return <h1>`Error! No data found: ${error.message}`</h1>;
-    if (!data?.getMostRecentHumidity) return <h1>No data found for {city_name}</h1>;
+    if (error) return <h1>Error! No data found: {error.message}</h1>;
+    if (!data?.getMostRecentHumidityByCity) return <h1>No data found for {city_name}</h1>;
     
     console.log("here's data: ", data);
     return(
         <>
-            <h3>I Present to You {data.getMostRecentHumidity.__typename}</h3>
+            <h3>I Present to You {data.getMostRecentHumidityByCity.__typename}</h3>
             <h3>Of {city_name.substring(0, city_name.length-4)}, {state_name}</h3>
-            <h1>{data.getMostRecentHumidity.humidity}</h1>
+            <h1>{data.getMostRecentHumidityByCity.humidity}</h1>
         </>
     )
 }
