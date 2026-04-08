f2fecbf9a3b0e2185195443fe4c0ed643a030777
Ammar Hoque <aihoque2@illinois.edu>
2026-04-08 01:09:33 -0400

    something better than nothing

---
+++ frontend/src/components/USStateToolTip.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+
+const USStateToolTip = ({state, mode, data_vals, location}) =>{
+
+    return(
+        <div>
+            barnacles
+        </div>
+    )
+}
