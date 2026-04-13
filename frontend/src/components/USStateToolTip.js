239b412d523410bbbb7970862e766b5bd93a003b
Ammar Hoque <aihoque2@illinois.edu>
2026-04-12 23:50:28 -0400

    FOund the useState() variable needed when setting up the cloud bubbles when clicking on states

--- frontend/src/components/USStateToolTip.js
+++ frontend/src/components/USStateToolTip.js
@@ -3,9 +3,21 @@ import React, { useState, useEffect } from "react";
 
 const USStateToolTip = ({state, mode, data_vals, location}) =>{
 
+    const Opened, setOpened = useState(false);
+
+    if (Opened){
+        let tooltip = (<div>
+        </div>);
+    }
+    else{
+        let tooltip = null;
+    }
+
+
+
     return(
         <div>
-            barnacles
+            {tooltip}
         </div>
     )
 }
