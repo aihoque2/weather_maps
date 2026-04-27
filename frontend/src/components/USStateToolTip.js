feaa8cebec7bd0d8b269bf86eaf76f6802ac8408
Ammar Hoque <aihoque2@illinois.edu>
2026-04-27 03:01:20 -0400

    extract tooltip x and y values

--- frontend/src/components/USStateToolTip.js
+++ frontend/src/components/USStateToolTip.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 
-const USStateToolTip = ({us_state, mode, data_vals, location}) =>{
-
+const USStateToolTip = ({us_state, mode, data_vals, loca}) =>{
+    var x = loc.x;
+    var y = loc.y;
     let title= ""
     if (mode === "temperature") {
         title = "Temperature for ".concat(us_state);
